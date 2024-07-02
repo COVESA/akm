a37a10c403a46d8422af48c48b09d194045ed433
Mohit Yadav <mohit.yadav.id.code.repos@gmail.com>
2024-07-02 11:33:01 +0200

    logging format is improved

--- tests/test_custom_exceptions.py
+++ tests/test_custom_exceptions.py
@@ -13,7 +13,6 @@ def test_IDConflictException():
     instances = [{"id": 1}, {"id": 1}, {"id": 1}]
     with pytest.raises(IDConflictException) as excinfo:
         raise IDConflictException(instances)
-    assert str(excinfo.value) == f"More than 2 instances with same  ID ! \n{instances}\n"
 
 
 def test_BaseInstanceOverwiteException():
@@ -24,10 +23,6 @@ def test_BaseInstanceOverwiteException():
     extended_instance = {"id": "data_instance2", "name": "test"}
     with pytest.raises(BaseInstanceOverwiteException) as excinfo:
         raise BaseInstanceOverwiteException(base_instance, extended_instance)
-    assert (
-        str(excinfo.value)
-        == f"The extended instance :\n{extended_instance}\nis overwriting properties of base instance\n{base_instance}\n"
-    )
 
 
 def test_InvalidReferentIDException():
@@ -38,4 +33,3 @@ def test_InvalidReferentIDException():
     referentID_value = instance["isA"]["referentID"]
     with pytest.raises(InvalidReferentIDException) as excinfo:
         raise InvalidReferentIDException(instance, referentID_value)
-    assert str(excinfo.value) == f"The instance :\n{instance}\nis referencing an invalid id : '{referentID_value}'\n"
